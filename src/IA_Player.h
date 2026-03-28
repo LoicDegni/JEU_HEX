04ad7deb74ab095a63b00fb32b9b6a36a275a150
Kaikou Loic Degni <>
2026-03-28 17:06:57 -0400

    Maj IA_Player

--- src/IA_Player.h
+++ src/IA_Player.h
@@ -25,22 +25,15 @@ class IA_Player : public Player_Interface {
 
         int visits = 0;
         double wins = 0;
+        // RAVE 
+        int raveVisits = 0;
+        double raveWins = 0;
 
         std::vector<std::pair<int,int>> untriedMoves;
 
         int moveRow, moveCol;
         char player;
     };
-
-    //-------------------DÉCLARATION DES FONCTIONS PRIVÉES-------------------//
-    Node* select(Node* node);
-    Node* expand(Node* node);
-    //char simulate(Hex_Environement& state);
-    void backpropagate(Node* node, char winner);
-    Node* bestChild(Node* node);
-    std::vector<std::pair<int,int>> getAllMoves(Hex_Environement& hex);
-
-
     //-------------------DÉFINITION DES FONCTIONS PRIVÉES-------------------//
     Node* select(Node* node) {
         double C = 1.41; //(2)^1/2 = 1.1414...
@@ -49,12 +42,20 @@ class IA_Player : public Player_Interface {
         double bestValue = -1e9;
 
         for(auto child: node->children) {
-            double uct = (child->wins / (child->visits + 1e-6)) +
-                         C * sqrt(log(node->visits + 1) / (child->visits + 1e-6));
-            if(uct > bestValue) {
-                bestValue = uct;
+            double rave = 0.0;
+            double uct = (child->wins / (child->visits + 1e-6)) + C * sqrt(log(node->visits + 1) / (child->visits + 1e-6));   //log(1) = 0
+            if(child->raveVisits > 0) 
+            {
+                rave = child->raveWins / child->raveVisits;
+            }
+            double X = 4 * pow(0.001,0.001) * child->visits * child->raveVisits; // 4 ×b2 ×S[i].nb ×ni
+            double beta = child->raveVisits / (child->visits + child->raveVisits + X + 1e-6);
+            double value = (1 - beta) * uct + beta * rave;
+            if (value > bestValue) 
+            {
                 best = child;
             }
+
         }
         return best;
     }
@@ -77,27 +78,41 @@ class IA_Player : public Player_Interface {
         return child;
     }
 
-    char simulate(Hex_Environement state) {
+    std::pair<char,std::vector<std::pair<int,int>>> simulate(Hex_Environement state) {
+        std::vector<std::pair<int,int>> played_move;
         while(!state.isGameOver()) {
             auto moves = getAllMoves(state);
             auto move = moves[rand()];
+            played_move.push_back(move);
             state.playMove(move.first, move.second);
         }
-        return state.getWinner();
+        return {state.getWinner(), played_move};
     }
 
-    void backpropagate(Node* node, char winner) {
+    void backpropagate(Node* node, char winner, std::vector<std::pair<int,int>> playedMoves) {
         while(node != nullptr) {
             node->visits++;
 
             if(node->player == winner) {
                 node->wins++;
             }
+            for(auto child : node->children) {
+                for(auto move : playedMoves) {
+                    if(child->moveRow == move.first && child->moveCol == move.second) {
+                        child->raveVisits++;
+
+                        if(child->player == winner) {
+                            child->raveWins++;
+                        }
+                    }
+
+                }
+            }
             node = node->parent;
         }
     }
 
-    Node* bestchild(Node* node) {
+    Node* FindBestChild(Node* node) {
         Node* best = nullptr;
         int maxVisits = -1;
 
@@ -123,7 +138,6 @@ class IA_Player : public Player_Interface {
         return moves;
     }
      
-
 public:
     IA_Player(char player, unsigned int taille=10) : _player(player), _taille(taille) {
         assert(player == 'X' || player == 'O');
@@ -142,25 +156,20 @@ public:
         while (std::chrono::steady_clock::now() - start < std::chrono::milliseconds(1900)) {
 
             Node* node = root;
-
             // 1. SELECTION
             while(node->untriedMoves.empty() && !node->children.empty()) {
                 node = select(node);
             }
-
             // 2. EXPANSION
             if(!node->untriedMoves.empty()) {
                 node = expand(node);
             }
-
             // 3. SIMULATION
-            char winner = simulate(node->state);
-
+            auto result = simulate(node->state);
             // 4. BACKDROP
-            backpropagate(node,winner);
+            backpropagate(node, result.first, result.second);
         }
-        Node* best = bestChild(root);
-
+        Node* best = FindBestChild(root);
         return {best->moveRow, best->moveCol};
     }
 };
