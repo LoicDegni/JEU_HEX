79603503fa12b25550ab2f8894c1f2396dbcb3f5
Kaikou Loic Degni <>
2026-03-31 00:36:11 -0400

    maj

--- src/IA_Player.h
+++ src/IA_Player.h
@@ -245,6 +245,7 @@ class IA_Player : public Player_Interface {
             std::cerr << "check point simulation apres applyUF\nLa taille de available est :" <<available.size() << "\nLe nombre de coup aleatoire joue est : " <<nb_coup <<"\n" << std::endl;
             std::cerr << "Le dernier coup joue est : (" << row << "," << col << ")\n" << std::endl;
         } while (!uf.hasWinner(pl));
+        std::cerr << "Le gagnant est : " << pl << std::endl;
 
         return pl;
     }
