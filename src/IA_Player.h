47a2bbb960c81da01a485054bbadcec038629199
Kaikou Loic Degni <>
2026-04-02 17:54:18 -0400

    maj

--- src/IA_Player.h
+++ src/IA_Player.h
@@ -218,7 +218,7 @@ private:
 
 //-------------------ALGO MCTS-------------------//
     Node* select(Node* node) {
-        double C = 0.82;
+        double C = 0.84;
         Node* best = nullptr;
         double bestValue = -1e9;
 
